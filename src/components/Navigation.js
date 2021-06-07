5cbf66fec4a520745c966d584793e1ea22e32063
novello99 <novellosal@gmail.com>
2021-06-06 21:04:00 -0500

    All Pages created

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,6 @@
+import { AppBar, Toolbar } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import {
   ACTIVITIES_ROUTE,
@@ -7,23 +9,45 @@ import {
   ROUTINES_ROUTE,
   LOGIN_ROUTE,
   REGISTER_ROUTE,
+  CREATE_ACTIVITY,
 } from "../constants";
 
 const Navigation = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem("token"))) {
+      setAuthenticated(true);
+    } else {
+      setAuthenticated(false);
+    }
+  }, []);
+
   return (
     <>
-      <Link to={HOME_ROUTE}>Home</Link>
-      <Link to={ROUTINES_ROUTE}>Routines</Link>
-      <Link to={MY_ROUTINES_ROUTE}>My Routines</Link>
-      <Link to={ACTIVITIES_ROUTE}>Activities</Link>
-      <div>
-        <Link to={LOGIN_ROUTE}>
-          <button>Login</button>
-        </Link>
-        <Link to={REGISTER_ROUTE}>
-          <button>Register</button>
-        </Link>
-      </div>
+      <AppBar>
+        <Toolbar>
+          {authenticated && (
+            <>
+              <Link to={HOME_ROUTE}>Home</Link>
+              <Link to={ROUTINES_ROUTE}>Routines</Link>
+              <Link to={MY_ROUTINES_ROUTE}>My Routines</Link>
+              <Link to={ACTIVITIES_ROUTE}>Activities</Link>
+              <Link to={CREATE_ACTIVITY}>
+                <button>Create Activity</button>
+              </Link>
+            </>
+          )}
+          <div>
+            <Link to={LOGIN_ROUTE}>
+              <button>Login</button>
+            </Link>
+            <Link to={REGISTER_ROUTE}>
+              <button>Register</button>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
     </>
   );
 };
--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,4 +1,5 @@
 import { Route } from "react-router-dom";
+import "./Pages.css";
 import {
   ACTIVITIES_ROUTE,
   HOME_ROUTE,
@@ -6,33 +7,52 @@ import {
   MY_ROUTINES_ROUTE,
   LOGIN_ROUTE,
   REGISTER_ROUTE,
+  CREATE_ACTIVITY,
 } from "../constants";
 
 import Login from "./Login";
+import Register from "./Register";
+import MyRoutines from "./MyRoutines";
+import Routines from "./Routines";
+import CreateActivityForm from "./CreateActivity";
 
-const Pages = (props) => {
-  const { setUserLogin } = props;
+const Pages = () => {
   return (
-    <>
+    <div className="pages">
       <Route path={HOME_ROUTE}>
         <h1>Home Page</h1>
       </Route>
       <Route path={ROUTINES_ROUTE}>
-        <h1>Routines Page</h1>
+        <Routines />
       </Route>
       <Route path={MY_ROUTINES_ROUTE}>
-        <h1>My Routines Page</h1>
+        <MyRoutines />
       </Route>
       <Route path={ACTIVITIES_ROUTE}>
         <h1>Activities Page</h1>
       </Route>
+      <Route path={CREATE_ACTIVITY}>
+        <CreateActivityForm />
+        {/* <h1> Create Activity</h1> */}
+      </Route>
+
       <Route path={LOGIN_ROUTE}>
         <Login />
+        {/* {JSON.parse(localStorage.getItem("token")) ? (
+          <Redirect to={<MyRoutines />} />
+        ) : (
+          <Login />
+        )} */}
       </Route>
       <Route path={REGISTER_ROUTE}>
-        <h1>Register</h1>
+        <Register />
+        {/* {JSON.parse(localStorage.getItem("token")) ? (
+          <Redirect to={<MyRoutines />} />
+        ) : (
+          <Register />
+        )} */}
       </Route>
-    </>
+    </div>
   );
 };
 
